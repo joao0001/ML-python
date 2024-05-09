0589a6660cbc24805967a3bfe406a1d9d260335b
joao vitor <54691079+joao0001@users.noreply.github.com>
2024-05-09 20:56:49 -0300

    Add files via upload

---
+++ variaveis.py
@@ -0,0 +1,8 @@
+x=5
+y="sou uma variavel"
+#note que o python não possui comandos para declarar variaveis
+
+print(x)
+print(y)
+#o retorno no console será os valores atribuidos a cada variavel
+#experimente comandos ex print(x+x) o resultado será 10
