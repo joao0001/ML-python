ba0f00ec2415bbc338fa932f1c67d50348c02baa
joao vitor <54691079+joao0001@users.noreply.github.com>
2024-05-14 22:08:51 -0300

    Add files via upload

---
+++ atribuir_valores.py
@@ -0,0 +1,6 @@
+#No exemplo abaixo veremos como atribuir multiplas variaveis utilizando uma linha
+x, y, z="teste", "variaveis", "ok"
+#diferente de outras linguagens não é necessario utilizar o ponto final
+print(x)
+print(y)
+print(z)
