09ce9c23ef377b63e28a605c97386a1e18c0bdab
joao vitor <54691079+joao0001@users.noreply.github.com>
2024-05-14 22:19:40 -0300

    Add files via upload

---
+++ descompactando.py
@@ -0,0 +1,8 @@
+#criando uma lista e adicionando os seus devidos valores
+carros=["mercedes", "ferrari", "porsche"]
+#atribuindo os valores da lista
+x, y, z=carros
+#print
+print(x)
+print(y)
+print(z)
