220e6bf4f255daa292180fd65036e311e1921fd9
joao vitor <54691079+joao0001@users.noreply.github.com>
2024-05-14 22:13:51 -0300

    atribuir_valores.py

--- atribuir_valores.py
+++ atribuir_valores.py
@@ -1,5 +1,6 @@
 #No exemplo abaixo veremos como atribuir multiplas variaveis utilizando uma linha
 x, y, z="teste", "variaveis", "ok"
+#para declarar um mesmo valor para diversas variaveis. exemplo a seguir = x = y= z="unico_valor"
 #diferente de outras linguagens não é necessario utilizar o ponto final
 print(x)
 print(y)
